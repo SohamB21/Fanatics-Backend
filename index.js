fbd6af9dfe4613d48a64c8e878e679ce01501988
Soham Banik <soham0110banik@gmail.com>
2024-04-15 23:35:18 +0530

    [fix]: env test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const cors = require("cors");
 
 // middleware (to connect with the frontend)
